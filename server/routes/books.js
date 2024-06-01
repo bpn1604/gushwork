14cabe86089768ee046e59676bce89b68d2053f0
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2024-06-01 17:47:46 +0530

    updating

--- server/routes/books.js
+++ server/routes/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
+
 mongoose.connect('mongodb://localhost:27017/bookreviews', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
